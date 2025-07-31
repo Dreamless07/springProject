ccdfc41474c6575f487c340ebe9bec4fcfb2caac
Dreamless07 <saadkhan7903@gmail.com>
2025-07-31 13:24:45 +0530

    added connections to repo layer and used FI with run

--- src/main/java/com/group1/springProject/SpringProjectApplication.java
+++ src/main/java/com/group1/springProject/SpringProjectApplication.java
@@ -1,13 +1,26 @@
+//main application where we can insert if required, we have connected this to repository layer
 package com.group1.springProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringProjectApplication {
-
+public class SpringProjectApplication implements CommandLineRunner{//inbuilt functional interface which has run as default method, we can insert values from here also
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+	@Autowired
+	private empdetailsRepository empdetailsRepository;//has all methods
+
+	@Override
+	public void run(String... args) throws Exception {
+		employeeClass emp1=new employeeClass();
+		emp1.setName("Ajay");//inbuilt method from repository layer
+		emp1.setAddress("Bangalore");
+		empdetailsRepository.save(emp1);//inbuilt method from repository layer(for insert)
 	}
 
 }
