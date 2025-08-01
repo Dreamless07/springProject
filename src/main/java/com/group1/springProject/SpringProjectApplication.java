24e6c4a4ff9bc0b9775f2677901ad16f9978a206
Dreamless07 <saadkhan7903@gmail.com>
2025-08-01 12:44:58 +0530

    removed the manual insert data

--- src/main/java/com/group1/springProject/SpringProjectApplication.java
+++ src/main/java/com/group1/springProject/SpringProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringProjectApplication implements CommandLineRunner{//inbuilt functional interface which has run as default method, we can insert values from here also
+public class SpringProjectApplication {//CommandLineRunner is inbuilt functional interface which has run as default method, we can insert values from here also
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
 	}
@@ -15,12 +15,14 @@ public class SpringProjectApplication implements CommandLineRunner{//inbuilt fun
 	@Autowired
 	private empdetailsRepository empdetailsRepository;//has all methods
 
-	@Override
+//Commented out just because we can perform CRUD from Postman
+
+	/*@Override
 	public void run(String... args) throws Exception {
 		employeeClass emp1=new employeeClass();
 		emp1.setName("Ajay");//inbuilt method from repository layer
 		emp1.setAddress("Bangalore");
 		empdetailsRepository.save(emp1);//inbuilt method from repository layer(for insert)
-	}
+	}*/
 
 }
